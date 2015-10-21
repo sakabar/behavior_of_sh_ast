faae5d452ea17ae2e319f8ece233fd9c29e96b38
sakaki <takafumi.sakakibara.pdx@gmail.com>
2015-10-21 18:49:19 +0900

    Modify: change message

--- exe.sh
+++ exe.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "Files in this dir is: *" | \
-while read line; do
-  echo 'If unquoted (echo $line), the files in the directory is printed (replaced in *)'
-  echo  $line  #If unquoted, the files in the directory is printed (replaced in '*')
-  echo ""
-  echo 'If quoted (echo $line), the original string is printed'
-  echo "$line" #If quoted, the original string is printed
-done
+line="Files in this dir is: *"
+echo '1  If unquoted (echo $line), the files in the directory is printed (replaced in *)'
+echo  "    "'echo  $line  => '$line
+
+echo ""
+
+echo '2  If quoted (echo "$line"), the original string is printed'
+echo "    "'echo "$line" => '"$line"
